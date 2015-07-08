3a40ffacd575a5d8ff94cc583a287d71fc295697
crazcalm <crazcalm@gmail.com>
2015-07-07 21:06:23 -0400

    Messed with the layout

--- bin/TicTacToe.rb
+++ bin/TicTacToe.rb
@@ -4,12 +4,14 @@ class TicTacToe #(change name)
 	include GladeGUI
 
 	def before_show()
-		@button1 = "Hello World"
+		@builder["window1"].title = "Tic Tac Toe"
 	  @keys = ["None"] * 9
+		@builder["player_label"].label = "Default Value"
 	end	
 
 	def keys__clicked(button)
 		button.label = button.label == "None" ? "YES" : "None"
+		@builder["player_label"].label = @builder["player_label"].label == "Player1"? "Player2" : "Player1"
 	end
 
 end
