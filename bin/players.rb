05e9beda5693c69ed7e0f7879e5c5f28ecc49f94
crazcalm <crazcalm@gmail.com>
2015-07-11 13:17:50 -0400

    Wrote some experimental code for my Stack, which will be use to save the moves

--- bin/players.rb
+++ bin/players.rb
@@ -1,6 +1,39 @@
 # Need to create a factory for Players
-def define_players(player1, player2)
-	
+def players_factory(player1, player2, player1_marker_x=true)
+  players = []
+
+	# Cases of initializing the players
+	if player1_marker
+
+		# Case 1
+		if player1 == "human"
+			players << Player.new("X")
+		else
+			players << PlayerBot.new("X")
+		end
+
+		if player2 == "human"
+			players << Player.new("O")
+		else
+			players << PlayerBot.new("O")
+		end
+
+	else
+		# Case2
+		if player1 == "human"
+			players << Player.new("O")
+		else
+			players << PlayerBot.new("O")
+		end
+
+		if player2 == "human"
+			players << Player.new("X")
+		else
+			players << PlayerBot.new("X")
+		end
+	end
+
+	players
 end
 
 class Player
---
+++ bin/queue.rb
@@ -0,0 +1,11 @@
+# I think is this how you create a one off class in Ruby.
+# Must try out
+#
+#
+#
+#
+#
+#
+
+
+class Stack < Array; end
